479f4e9020eebd69bd6276b2fa6d38b39555e48e
jamalie <jamaliethenor@gmail.com>
2017-01-18 12:37:57 -0500

    Added Bunny animations
    Bunny running and jumping animations

--- Assets/BimmyController.cs
+++ Assets/BimmyController.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class BimmyController : MonoBehaviour {
 
     private Rigidbody2D myRigidBody;
+    private Animator myAnim;
     public float bunnyJumpForce = 500f;
 
 	// Use this for initialization
     // This will look on the current game object and find the rigid2d component that is attached to it and assign it to the rigidbody variable
 	void Start () {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
 		
 	}
 	
@@ -20,5 +22,6 @@ public class BimmyController : MonoBehaviour {
         {
             myRigidBody.AddForce(transform.up * bunnyJumpForce);
         }
+        myAnim.SetFloat("vVelocity", myRigidBody.velocity.y);
 	}
 }
